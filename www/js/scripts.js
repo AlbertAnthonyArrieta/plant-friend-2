2e2da572e2ac93eb6e1f648cd93850b904c1d299
chris.ann <chrstn.clark@gmail.com>
2022-10-24 10:27:31 -0600

    started to add plant behavior

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -5,8 +5,30 @@ var app = new Framework7({
 });
 var mainView = app.views.create('.view-main')
 
+var waterlevel = 20;
+var nowater = 0;
+var drowning = 40;
+
+var neardeath = false;
+
+function dryout() {
+  waterlevel--;
+  console.log(waterlevel)
+  checkhealth();
+  var watertimer = setTimeout(dryout, 500);
+}
+dryout();
+
+function checkhealth() {
+  if (waterlevel <= nowater) {
+    neardeath = true;
+    console.log("Help!")
+    $("#plant path").css("fill", "chocolate")
+  }
+}
+
 // by default:
-// - plant starts health
+// - plant starts healthy
 // - dry out over time
 // - deplete in nutrients over time
 
