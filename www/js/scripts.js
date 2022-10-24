98bce825eb66ab81a23180e49bd635a1cf2bc2f8
chris.ann <chrstn.clark@gmail.com>
2022-10-24 11:21:17 -0600

    added event listeners

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -5,11 +5,18 @@ var app = new Framework7({
 });
 var mainView = app.views.create('.view-main')
 
+// water level thresholds
 var waterlevel = 20;
 var nowater = 0;
 var drowning = 40;
 
+// nutrient level thresholds
+var food = 30;
+var nofood = 0;
+var overfeed = 40;
+
 var neardeath = false;
+var trimmed = false;
 
 function dryout() {
   waterlevel--;
@@ -19,13 +26,47 @@ function dryout() {
 }
 dryout();
 
+function starving() {
+  food--;
+  //console.log(food);
+  checkhealth();
+  var foodtimer = setTimeout(starving, 500);
+}
+starving();
+
 function checkhealth() {
-  if (waterlevel <= nowater) {
+  if (waterlevel <= nowater || waterlevel >= drowning || food <= nofood) {
     neardeath = true;
     console.log("Help!")
     $("#plant path").css("fill", "chocolate")
   }
+  if (neardeath && waterlevel > nowater && food > nofood && trimmed) {
+    $("#plant path").css("fill", "#568b62")
+    neardeath = false;
+    setTimeout(function () {
+      $("#trim").fadeIn();
+      trimmed = false;
+    }, 5000)
+  }
 }
+
+$("#water-me").on("click", function () {
+  waterlevel += 20;
+  $("#water").fadeIn().delay(3000).fadeOut()
+})
+
+$("#feed-me").on("click", function () {
+  food += 20;
+  $("#food").fadeIn().delay(3000).fadeOut()
+})
+
+$("#trim-me").on("click", function () {
+  trimmed = true;
+  $("#scissors").fadeIn().delay(2000).fadeOut();
+  $("#trim").fadeOut();
+})
+
+
 
 // by default:
 // - plant starts healthy
